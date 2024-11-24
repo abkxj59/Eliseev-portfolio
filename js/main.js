7e99701d2240cd3038a49e34b1c1c75b94da4c6a
Abk <flocktory@Noutbuk-flocktory.local>
2024-11-25 04:28:33 +0500

    Добавляет кэширование картинок

--- js/main.js
+++ js/main.js
@@ -1,46 +1,8 @@
-import {initMap} from './modules/map.js';
-import {initializeMenu} from './modules/menu.js';
-import {replacePrettyPrices} from './utils/price-prettifiy.js';
-import {initPromocode} from './modules/promocode.js';
-import {initCards} from './modules/cards.js';
-import {initFormSubmit} from './modules/form-submit.js';
-import {initModal} from './modules/modal.js';
+import { cachePictures } from './modules/cachePictures.js';
 // ---------------------------------
 
 window.addEventListener('DOMContentLoaded', () => {
-  replacePrettyPrices();
-  initializeMenu();
-
-  ymaps.ready(initMap);
   window.addEventListener('load', () => {
-    initPromocode();
-    initCards();
-    initFormSubmit();
-    initModal();
+    cachePictures();
   });
 });
-
-// ---------------------------------
-
-// ❗❗❗ обязательно установите плагины eslint, stylelint, editorconfig в редактор кода.
-
-// привязывайте js не на классы, а на дата атрибуты (data-validate)
-
-// вместо модификаторов .block--active используем утилитарные классы
-// .is-active || .is-open || .is-invalid и прочие (обязателен нейминг в два слова)
-// .select.select--opened ❌ ---> [data-select].is-open ✅
-
-// выносим все в дата атрибуты
-// url до иконок пинов карты, настройки автопрокрутки слайдера, url к json и т.д.
-
-// для адаптивного JS используейтся matchMedia и addListener
-// const breakpoint = window.matchMedia(`(min-width:1024px)`);
-// const breakpointChecker = () => {
-//   if (breakpoint.matches) {
-//   } else {
-//   }
-// };
-// breakpoint.addListener(breakpointChecker);
-// breakpointChecker();
-
-// используйте .closest(el)
